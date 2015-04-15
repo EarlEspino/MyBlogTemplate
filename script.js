8e453044f1cc2a1cf0895444c930bcd22283c777
EarlEspino <ee760971@wcupa.edu>
2015-04-14 23:24:12 -0400

    added css and script. hover function for nav links

---
+++ script.js
@@ -0,0 +1,28 @@
+$(function(){
+	/*
+	$('.navLink').mouseenter(function(){
+		$('.navLink').fadeTo(100,.5);
+	});
+
+	$('.navLink').mouseleave(function(){
+		$('.navLink').fadeTo(100,1);
+	});
+*/
+	$('#nav1').hover(function(){
+		$('#nav1').fadeTo(200,.5);
+	}, function(){
+		$('#nav1').fadeTo(200,1);
+	});
+
+	$('#nav2').hover(function(){
+		$('#nav2').fadeTo(200,.5);
+	}, function(){
+		$('#nav2').fadeTo(200,1);
+	});
+
+	$('#nav3').hover(function(){
+		$('#nav3').fadeTo(200,.5);
+	}, function(){
+		$('#nav3').fadeTo(200,1);
+	});
+});
